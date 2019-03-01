a95c56765b2365e980fff649ceb90e11da75a105
Marc Doerflinger <github@doerf.li>
2019-03-02 00:08:06 +0100

    no springboottest

--- customer/src/test/kotlin/li/doerf/microstore/customer/services/CustomerServiceTest.kt
+++ customer/src/test/kotlin/li/doerf/microstore/customer/services/CustomerServiceTest.kt
@@ -2,19 +2,18 @@ package li.doerf.microstore.customer.services
 
 import li.doerf.microstore.customer.repositories.CustomerRepository
 import li.doerf.microstore.dto.CustomerCreate
-import li.doerf.microstore.test.BaseTestWithKafkaAndRedis
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Ignore
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
 
-@Ignore
-@SpringBootTest
-class CustomerServiceTest @Autowired constructor(
-        val customerService: CustomerService,
-        val customerRepository: CustomerRepository
-) : BaseTestWithKafkaAndRedis() {
+@ExtendWith(MockitoExtension::class)
+class CustomerServiceTest {
+
+    @Mock
+    private lateinit var customerRepository: CustomerRepository
 
     @Test
     fun testCreate() {
@@ -24,14 +23,15 @@ class CustomerServiceTest @Autowired constructor(
                 "Body"
         )
 
+        val customerService = CustomerService(customerRepository)
+
         val cust = customerService.create(customerToCreate)
         assertThat(cust.id).isNotNull()
         assertThat(cust.email).isEqualTo(customerToCreate.email)
         assertThat(cust.firstname).isEqualTo(customerToCreate.firstname)
         assertThat(cust.lastname).isEqualTo(customerToCreate.lastname)
 
-        val newRec = customerRepository.findById(cust.id)
-        assertThat(newRec).isNotNull
+        Mockito.verify(customerRepository).save(Mockito.any())
     }
 
 }
