bcb41534551cf34d99d2975cefda5793305d5284
Marc Doerflinger <github@doerf.li>
2019-03-02 21:12:48 +0100

    add test

--- api/src/test/kotlin/li/doerf/microstore/api/controllers/CustomerControllerTest.kt
+++ api/src/test/kotlin/li/doerf/microstore/api/controllers/CustomerControllerTest.kt
@@ -5,13 +5,17 @@ import li.doerf.microstore.api.rest.dto.CreateCustomerRequest
 import li.doerf.microstore.dto.CustomerCreated
 import li.doerf.microstore.services.KafkaService
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.fail
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.http.HttpStatus
+import org.springframework.web.server.ResponseStatusException
 import java.util.*
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.CompletionException
 
 @ExtendWith(MockitoExtension::class)
 class CustomerControllerTest {
@@ -40,17 +44,44 @@ class CustomerControllerTest {
                 "some",
                 "Body"
         )
-        val future = CompletableFuture.supplyAsync{cc}
+        val future = CompletableFuture.supplyAsync<CustomerCreated>{cc}
         Mockito.`when`(customersListener.registerCorrelationIdForResponse(any())).thenReturn(future)
         val customerController = CustomerController(kafkaService, customersListener)
 
         val res = customerController.create(req)
-        val resp = res.get()
+        val resp = res.join()
 
         assertThat(resp.id).isEqualTo(uuid)
         assertThat(resp.email).isEqualTo("some@body.com")
         assertThat(resp.firstname).isEqualTo("some")
         assertThat(resp.lastname).isEqualTo("Body")
+    }
+
+    @Test
+    fun testCreateWithException() {
+        val req = CreateCustomerRequest(
+                "some@body.com",
+                "some",
+                "Body"
+        )
+        val uuid = UUID.randomUUID().toString()
+        val cc = CustomerCreated(
+                uuid,
+                "some@body.com",
+                "some",
+                "Body"
+        )
+        val future = CompletableFuture.supplyAsync<CustomerCreated>{throw IllegalArgumentException("something happened")}
+        Mockito.`when`(customersListener.registerCorrelationIdForResponse(any())).thenReturn(future)
+        val customerController = CustomerController(kafkaService, customersListener)
+
+        try {
+            val res = customerController.create(req)
+            res.join()
+            fail<String>("expected exception")
+        } catch(e: CompletionException) {
+            assertThat((e.cause as ResponseStatusException).status).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR)
+        }
     }
 
 }
