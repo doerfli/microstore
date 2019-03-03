53a8e8e78c735060593b3bfa52098ca2b165a750
Marc Doerflinger <github@doerf.li>
2019-03-03 14:35:20 +0100

    refactor

--- common/src/main/kotlin/li/doerf/microstore/listeners/ReplayingRecordsListener.kt
+++ common/src/main/kotlin/li/doerf/microstore/listeners/ReplayingRecordsListener.kt
@@ -1,10 +1,14 @@
 package li.doerf.microstore.listeners
 
 import li.doerf.microstore.utils.getLogger
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.common.TopicPartition
+import org.springframework.kafka.annotation.KafkaHandler
 import org.springframework.kafka.listener.ConsumerSeekAware
+import org.springframework.kafka.support.KafkaHeaders
+import org.springframework.transaction.annotation.Transactional
 
-open class ReplayingRecordsListener : ConsumerSeekAware {
+abstract class ReplayingRecordsListener : ConsumerSeekAware {
 
     companion object {
         @Suppress("JAVA_CLASS_ON_COMPANION")
@@ -16,8 +20,25 @@ open class ReplayingRecordsListener : ConsumerSeekAware {
      */
     private val initialOffsets: MutableMap<Int,Long> = mutableMapOf()
 
-    override fun onIdleContainer(assignments: MutableMap<TopicPartition, Long>?, callback: ConsumerSeekAware.ConsumerSeekCallback?) {
+    @Transactional
+    @KafkaHandler(isDefault = true)
+    open fun receive(record: ConsumerRecord<String, Any>) {
+        log.debug("received: $record")
+        val correlationId =
+                String(record.headers().headers(KafkaHeaders.CORRELATION_ID).first().value())
+        val event = record.value()
+        val eventResponse = applyEventToStore(event, correlationId)
+        if(isReplaying(record.partition(), record.offset())) {
+            return
+        }
+        processCommand(event, correlationId, eventResponse)
+    }
 
+    protected abstract fun applyEventToStore(event: Any?, correlationId: String): Any?
+
+    protected abstract fun processCommand(event: Any, correlationId: String, eventResponse: Any?)
+
+    override fun onIdleContainer(assignments: MutableMap<TopicPartition, Long>?, callback: ConsumerSeekAware.ConsumerSeekCallback?) {
     }
 
     override fun onPartitionsAssigned(assignments: MutableMap<TopicPartition, Long>?, callback: ConsumerSeekAware.ConsumerSeekCallback?) {
@@ -32,10 +53,9 @@ open class ReplayingRecordsListener : ConsumerSeekAware {
     }
 
     override fun registerSeekCallback(callback: ConsumerSeekAware.ConsumerSeekCallback?) {
-
     }
 
-    fun isReplaying(partition: Int, offset: Long): Boolean {
+    private fun isReplaying(partition: Int, offset: Long): Boolean {
         val initialOffset = initialOffsets.getOrDefault(partition, 0)
         return offset < initialOffset
     }
--- customer/src/main/kotlin/li/doerf/microstore/customer/listeners/CustomersListener.kt
+++ customer/src/main/kotlin/li/doerf/microstore/customer/listeners/CustomersListener.kt
@@ -8,13 +8,11 @@ import li.doerf.microstore.entities.Customer
 import li.doerf.microstore.listeners.ReplayingRecordsListener
 import li.doerf.microstore.services.KafkaService
 import li.doerf.microstore.utils.getLogger
-import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.support.KafkaHeaders
 import org.springframework.stereotype.Component
-import org.springframework.transaction.annotation.Transactional
 
+@KafkaListener(topics = [TOPIC_CUSTOMERS])
 @Component
 class CustomersListener @Autowired constructor(
         val customerService: CustomerService,
@@ -26,28 +24,14 @@ class CustomersListener @Autowired constructor(
         private val log = getLogger(javaClass)
     }
 
-    @KafkaListener(topics = [TOPIC_CUSTOMERS])
-    @Transactional
-    fun receive(record: ConsumerRecord<String, Any>) {
-        log.debug("received: $record")
-        val correlationId =
-                String(record.headers().headers(KafkaHeaders.CORRELATION_ID).first().value())
-        val event = record.value()
-        val eventResponse = applyEventToStore(event, correlationId)
-        if(isReplaying(record.partition(), record.offset())) {
-            return
-        }
-        processCommand(event, correlationId, eventResponse)
-    }
-
-    private fun applyEventToStore(event: Any?, correlationId: String): Any? {
+    override fun applyEventToStore(event: Any?, correlationId: String): Any? {
         when(event) {
             is CustomerCreate -> return customerService.create(event)
         }
         return null
     }
 
-    private fun processCommand(event: Any, correlationId: String, eventResponse: Any?) {
+    override fun processCommand(event: Any, correlationId: String, eventResponse: Any?) {
         log.debug("processCommand")
         when(event) {
             is CustomerCreate -> {
