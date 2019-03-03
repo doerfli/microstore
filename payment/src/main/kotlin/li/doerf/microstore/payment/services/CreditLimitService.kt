54b0043130e518f2f14bb8b4a8137ccf10a74787
Marc Doerflinger <github@doerf.li>
2019-03-03 20:49:07 +0100

    add log msg

--- payment/src/main/kotlin/li/doerf/microstore/payment/services/CreditLimitService.kt
+++ payment/src/main/kotlin/li/doerf/microstore/payment/services/CreditLimitService.kt
@@ -2,6 +2,7 @@ package li.doerf.microstore.payment.services
 
 import li.doerf.microstore.dto.kafka.CustomerIncreaseLimit
 import li.doerf.microstore.payment.repositories.CustomerRepository
+import li.doerf.microstore.utils.getLogger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
@@ -9,12 +10,18 @@ import org.springframework.stereotype.Service
 class CreditLimitService @Autowired constructor(
         val customerRepository: CustomerRepository
 ){
+    companion object {
+        @Suppress("JAVA_CLASS_ON_COMPANION")
+        private val log = getLogger(javaClass)
+    }
+
     fun increaseLimit(input: CustomerIncreaseLimit) {
         val customer = customerRepository.findById(input.id)
         if(customer.isEmpty) {
             throw IllegalArgumentException("customer with id ${input.id} unknown")
         }
         customer.get().creditLimit = customer.get().creditLimit.add(input.amount)
+        log.info("credit limit increased. $customer")
         customerRepository.save(customer.get())
     }
 }
