2417c42bb70399d023c172444f08ca9f3416cb52
Marc Doerflinger <github@doerf.li>
2019-03-20 22:55:10 +0100

    add hostname via variable

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
 export const AXIOS = axios.create({
-    baseURL: `http://localhost:8080`,
+    baseURL: `http://${process.env.VUE_APP_API_HOST}:8080`,
     headers: {
         // 'Access-Control-Allow-Origin': 'http://localhost:8070'
     }
 })
+
+console.log(process.env);
