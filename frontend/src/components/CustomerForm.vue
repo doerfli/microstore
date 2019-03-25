9c7075fac8ec6ce527b2d9067b3c1ade12c00be1
Marc Doerflinger <github@doerf.li>
2019-03-25 22:14:13 +0100

    fill form with random data

--- frontend/src/components/CustomerForm.vue
+++ frontend/src/components/CustomerForm.vue
@@ -4,19 +4,29 @@
     <input type="text" v-model="lastname" /><br/>
     <input type="text" v-model="email" /><br/>
     <button v-on:click="$emit('save', {firstname: firstname, lastname: lastname, email: email})">Save</button>
+    <button v-on:click="clear()">Clear</button>
   </div>
 </template>
 
 <script>
+  import faker from 'faker';
   export default {
-        name: "CustomerForm",
-        data: function () {
-            return {
-                firstname: "First name ...",
-                lastname: "Last name ...",
-                email: "Em@ail ..."
-            }
-        }
+      name: "CustomerForm",
+      data: function () {
+          return {
+              firstname: faker.name.firstName(),
+              lastname: faker.name.lastName(),
+              email: faker.internet.email()
+          }
+      },
+      methods: {
+          clear: function() {
+              this.firstname = "";
+              this.lastname = "";
+              this.email = "";
+          }
+      }
+
     }
 </script>
 
